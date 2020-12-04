858512d73a3d24e903bcaa976372f075a5e26234
rtetelbaum <rtetelbaum@mailbox.org>
2020-12-04 16:34:56 -0500

    fixed seeds again

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ user2 = User.create(first_name: 'Lee', last_name: 'Michaeli', email: 'leeisr@gma
 
 card1 = Card.create(image_url: 'https://i.ibb.co/cNx9R4V/birthday02.jpg', sender_name: 'Roman', sender_email: 'rtetelbaum@mailbox.org', recipient_email: 'grandma@gmail.com', subject: 'Happy Birthday', message: 'Happy Birthday Grandma! Love you!', user: user1)
 card2 = Card.create(image_url: 'https://i.ibb.co/CKrLhdX/christmas04.jpg', sender_name: 'Lee', sender_email: 'leeisr@gmail.com', recipient_email: 'grandpa@gmail.com', subject: 'Happy Christmas', message: 'Santa Claus is cool. So are you!', user: user2)
-card3 = Card.create(image_url: 'https://i.ibb.co/cNx9R4V/birthday03.jpg', sender_name: 'Roman', sender_email: 'rtetelbaum@mailbox.org', recipient_email: 'mom@gmail.com', subject: 'Happy Birthday', message: 'Happy Birthday Mom! Love you!', user: user1)
+card3 = Card.create(image_url: 'https://i.ibb.co/F8YcLvq/birthday03.jpg', sender_name: 'Roman', sender_email: 'rtetelbaum@mailbox.org', recipient_email: 'mom@gmail.com', subject: 'Happy Birthday', message: 'Happy Birthday Mom! Love you!', user: user1)
 card4 = Card.create(image_url: 'https://i.ibb.co/1mT2PXG/holidays05.jpg', sender_name: 'Lee', sender_email: 'leeisr@gmail.com', recipient_email: 'son@gmail.com', subject: 'Happy Holidays', message: 'I forgot what religion we raised you with. Happy holidays!', user: user2)
-card5 = Card.create(image_url: 'https://i.ibb.co/cNx9R4V/birthday07.jpg', sender_name: 'Roman', sender_email: 'rtetelbaum@mailbox.org', recipient_email: 'dad@gmail.com', subject: 'Happy Birthday', message: 'Happy Birthday Dad! Love you!', user: user1)
+card5 = Card.create(image_url: 'https://i.ibb.co/Zfdd9wV/birthday07.jpg', sender_name: 'Roman', sender_email: 'rtetelbaum@mailbox.org', recipient_email: 'dad@gmail.com', subject: 'Happy Birthday', message: 'Happy Birthday Dad! Love you!', user: user1)
 card6 = Card.create(image_url: 'https://i.ibb.co/7CnLVLg/holidays08.jpg', sender_name: 'Lee', sender_email: 'leeisr@gmail.com', recipient_email: 'daughter@gmail.com', subject: 'Happy Holidays', message: 'Ask your brother', user: user2)
 
 reminder1 = Reminder.create(text: 'Send grandpa birthday card', user: user1)
