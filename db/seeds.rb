4d70399a93f75836ca53bd9496df191be6bed650
rtetelbaum <rtetelbaum@mailbox.org>
2020-12-09 16:44:37 -0500

    updated seeds with destroy_all and added ds_store to gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+Card.destroy_all
+Reminder.destroy_all
+
 user1 = User.create(first_name: 'Roman', last_name: 'Tetelbaum', email: 'roman@tetelbaum.com')
 user2 = User.create(first_name: 'Lee', last_name: 'Michaeli', email: 'lee@michaeli.com')
 
